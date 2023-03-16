1e4a93529ccd94ca38d4c2aa9a87bd9ee2af713c
Diego Torres <diegotorre0303s@gmail.com>
2023-03-16 11:57:33 -0500

    initial commit

---
+++ .projenrc.js
@@ -0,0 +1,15 @@
+const { awscdk } = require('projen');
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Diego Torres',
+  authorAddress: 'diegotorre0303s@gmail.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  name: '22-dt-cdk-lib',
+  repositoryUrl: 'https://github.com/diegotorre0303s/22-dt-cdk-lib.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
