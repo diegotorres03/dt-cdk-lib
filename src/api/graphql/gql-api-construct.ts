a596e29f7b242eb12f56f558ac58561ff3124e8f
Diego Torres <diegotorre0303s@gmail.com>
2023-03-20 09:05:08 -0500

    fix: working rest api authorizer

--- src/api/graphql/gql-api-construct.ts
+++ src/api/graphql/gql-api-construct.ts
@@ -1,12 +1,102 @@
-
+import * as AppSync from 'aws-cdk-lib/aws-appsync'
 import { Construct } from 'constructs';
 
 
-export class AppSyncConstruct extends Construct {
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
-  }
+export class GraphQlApiConstruct extends Construct {
 
+    private api: AppSync.CfnGraphQLApi
+
+    private schemaParts: string[] = []
+
+    private currentOperation = ''
+
+    constructor(scope: Construct, id: string) {
+        super(scope, id);
+
+        const name = id + '_gql-api'
+
+        this.api = new AppSync.CfnGraphQLApi(this, name, {
+            name: name,
+            authenticationType: 'API_KEY', //  API_KEY, AWS_IAM, AMAZON_COGNITO_USER_POOLS, OPENID_CONNECT, or AWS_LAMBDA
+            xrayEnabled: true,
+            // logConfig: {}
+        })
+
+        const gql = this
+
+        gql
+            .addToSchema(`
+              
+type PatientData {
+    tenentId: ID!
+    resource: ID
+    Data: String
+}
+
+input PatientDataInput {
+    tenentId: ID!
+    resource: ID
+    Data: String
+}
+
+type PatientKey {
+    tenentId: ID!
+    resource: ID!
+}
+
+input PatientKeyInput {
+    tenentId: ID!
+    resource: ID!
+}
+
+type Query {
+    listPatients(tenentId: ID): [PatientData]
+    getPatient(key: PatientKeyInput): PatientData
+}
+
+type Mutation {
+    createPatient(resource: PatientDataInput!): PatientData
+}
+
+type Subscription {
+    onCreatePatient(patientFilter: PatientDataInput): PatientData
+    @aws_subscribe(mutations:["createPatient"])
+}
+
+            `)
+            .mutation('updateUser')
+            .query()
+            .subscription()
+            .fn()
+            .request((ev => ev).toString())
+            .response((ev => ev).toString())
+
+
+    }
+
+
+    addToSchema(fragment: string): GraphQlApiConstruct {
+        this.schemaParts.push(fragment)
+
+        return this
+    }
+
+    mutation(name: string): GraphQlApiConstruct {
+        this.currentOperation = 'mutation'
+
+        return this
+    }
+
+    query(name: string): GraphQlApiConstruct {
+        this.currentOperation = 'mutation'
+
+        return this
+    }
+    subscription(name: string): GraphQlApiConstruct {
+        this.currentOperation = 'mutation'
+
+        return this
+    }
 }
 
 
--- src/api/rest/rest-api-construct.ts
+++ src/api/rest/rest-api-construct.ts
@@ -3,7 +3,7 @@ import * as ApiGateway from 'aws-cdk-lib/aws-apigateway';
 import * as Dynamo from 'aws-cdk-lib/aws-dynamodb';
 import * as Lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
-import { FunctionConstruct } from '../../compute';
+import { FunctionConstruct, FunctionOptions } from '../../compute';
 // import { WebAppConstruct } from '../../webapp/webapp-construct'
 
 
@@ -20,192 +20,192 @@ import { FunctionConstruct } from '../../compute';
  */
 export class RestApiConstruct extends Construct {
 
-  private currentAuthorizer?: ApiGateway.RequestAuthorizer; // Lambda.Function
-  private currentHandler?: Lambda.Function;
-  private api: ApiGateway.RestApi;
+    private currentAuthorizer?: ApiGateway.RequestAuthorizer; // Lambda.Function
+    private currentHandler?: Lambda.Function;
+    private api: ApiGateway.RestApi;
 
 
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
+    constructor(scope: Construct, id: string) {
+        super(scope, id);
 
 
-    this.api = new ApiGateway.RestApi(this, id + '_api', {
-      deployOptions: { stageName: process.env.STAGE || 'dev' },
+        this.api = new ApiGateway.RestApi(this, id + '_api', {
+            deployOptions: { stageName: process.env.STAGE || 'dev' },
 
-    });
-
-
-    // const exampleAuthorizer = './path/to/auth'
-    // const exampleHandler = (ev => ({ statusCode: 204 })).toString()
-
-    // const dynamo = {} as Construct
-    // const api = this
-    // api
-    //     .cors() // this add default cors
-    //     .authorizer(exampleAuthorizer)
-    //     .get('/users', exampleHandler).readFrom(dynamo)
-    //     .post('/users/{userId}', exampleHandler).writeTo(dynamo)
+        });
 
 
-  }
+        // const exampleAuthorizer = './path/to/auth'
+        // const exampleHandler = (ev => ({ statusCode: 204 })).toString()
 
-  /**
-       * enable cors for this API
-       *
-       * @author Diego Tores <diegotorres0303@gmail.com>
-       *
-       * @param {ApiGateway.CorsOptions} [options]
-       * @return {*}  {RestApiConstruct}
-       * @memberof RestApiConstruct
-       */
-  cors(options?: ApiGateway.CorsOptions): RestApiConstruct {
-    const defaultOptions = {
-      allowOrigins: ApiGateway.Cors.ALL_ORIGINS,
-      allowMethods: ApiGateway.Cors.ALL_METHODS,
-      allowHeaders: [
-        'Content-type', 'X-Amz-Date', 'X-Api-Key', 'Authorization',
-        'Access-Controll-Allow-Headers', 'Access-Controll-Allow-Origins', 'Access-Controll-Allow-Methods',
-      ],
-      allowCredentials: true,
-    };
-    const corsOptions = options || defaultOptions;
-
-    this.api.root.addCorsPreflight(corsOptions);
-    return this;
-  }
-
-  /**
-       * add a webapp construct on cors
-       *
-       * @param {WebAppConstruct} webapp
-       * @return {*}  {RestApiConstruct}
-       * @memberof RestApiConstruct
-       */
-  // addToCors(webapp: WebAppConstruct): RestApiConstruct {
-  //     // const origin = webapp.
-  //     throw unimplementedError
-  //     return this
-  // }
-
-  /**
-       * create an authorizer and use it in the followin lambdas until a new authorizer is created
-       *
-       * @author Diego Tores <diegotorres0303@gmail.com>
-       *
-       * @param {string} handlerCode
-       * @return {*}  {RestApiConstruct}
-       * @memberof RestApiConstruct
-       */
-  authorizer(name: string, handlerCode: string): RestApiConstruct {
-    const authFn = new FunctionConstruct(this, `${this.api.restApiId}_${name}_authorizer`);
-    authFn.handler(handlerCode);
-
-    const authorizer = new ApiGateway.RequestAuthorizer(this, 'MyAuthorizer', {
-      handler: authFn.handlerFn,
-      identitySources: [
-        ApiGateway.IdentitySource.header('Authorization'),
-        ApiGateway.IdentitySource.queryString('allow'),
-      ],
-    });
-
-    this.currentAuthorizer = authorizer;
-
-    return this;
-  }
+        // const dynamo = {} as Construct
+        // const api = this
+        // api
+        //     .cors() // this add default cors
+        //     .authorizer(exampleAuthorizer)
+        //     .get('/users', exampleHandler).readFrom(dynamo)
+        //     .post('/users/{userId}', exampleHandler).writeTo(dynamo)
 
 
-  /**
-       * let the last created lambda hace read access to a given construct
-       *
-       * Supported targets:
-       * - DynamoDB
-       *
-       * @author Diego Tores <diegotorres0303@gmail.com>
-       *
-       * @param {Construct} construct
-       * @return {*}  {RestApiConstruct}
-       * @memberof RestApiConstruct
-       */
-  readFrom(construct: Construct): RestApiConstruct {
-    if (!this.currentHandler) throw new Error('you need to create a handler function first');
+    }
 
-    // if Dynamo
-    const table = construct as Dynamo.Table;
-    table.grantReadData(this.currentHandler);
+    /**
+         * enable cors for this API
+         *
+         * @author Diego Tores <diegotorres0303@gmail.com>
+         *
+         * @param {ApiGateway.CorsOptions} [options]
+         * @return {*}  {RestApiConstruct}
+         * @memberof RestApiConstruct
+         */
+    cors(options?: ApiGateway.CorsOptions): RestApiConstruct {
+        const defaultOptions = {
+            allowOrigins: ApiGateway.Cors.ALL_ORIGINS,
+            allowMethods: ApiGateway.Cors.ALL_METHODS,
+            allowHeaders: [
+                'Content-type', 'X-Amz-Date', 'X-Api-Key', 'Authorization',
+                'Access-Controll-Allow-Headers', 'Access-Controll-Allow-Origins', 'Access-Controll-Allow-Methods',
+            ],
+            allowCredentials: true,
+        };
+        const corsOptions = options || defaultOptions;
 
+        this.api.root.addCorsPreflight(corsOptions);
+        return this;
+    }
 
-    return this;
-  }
+    /**
+         * add a webapp construct on cors
+         *
+         * @param {WebAppConstruct} webapp
+         * @return {*}  {RestApiConstruct}
+         * @memberof RestApiConstruct
+         */
+    // addToCors(webapp: WebAppConstruct): RestApiConstruct {
+    //     // const origin = webapp.
+    //     throw unimplementedError
+    //     return this
+    // }
 
-  /**
-       * let the last created lambda hace write access to a given construct
-       *
-       * Supported targets:
-       * - DynamoDB
-       *
-       * @author Diego Tores <diegotorres0303@gmail.com>
-       *
-       * @param {Construct} construct
-       * @return {*}  {RestApiConstruct}
-       * @memberof RestApiConstruct
-       */
-  writeTo(construct: Construct): RestApiConstruct {
-    if (!this.currentHandler) throw new Error('you need to create a handler function first');
+    /**
+         * create an authorizer and use it in the followin lambdas until a new authorizer is created
+         *
+         * @author Diego Tores <diegotorres0303@gmail.com>
+         *
+         * @param {string} handlerCode
+         * @return {*}  {RestApiConstruct}
+         * @memberof RestApiConstruct
+         */
+    authorizer(name: string, handlerCode: string): RestApiConstruct {
+        const authFn = new FunctionConstruct(this, `${name}_authorizer`);
+        authFn.handler(handlerCode);
 
+        const authorizer = new ApiGateway.RequestAuthorizer(this, 'MyAuthorizer', {
+            handler: authFn.handlerFn,
+            identitySources: [
+                ApiGateway.IdentitySource.header('Authorization'),
+                ApiGateway.IdentitySource.queryString('allow'),
+            ],
+        });
 
-    // if Dynamo
-    const table = construct as Dynamo.Table;
-    table.grantWriteData(this.currentHandler);
+        this.currentAuthorizer = authorizer;
 
-
-    return this;
-  }
-
-  private createMethodIntegration(method: string, path: string, handlerCode: string) {
-
-    console.log('use authorizer here', this.currentAuthorizer);
-    const fn = new FunctionConstruct(this, `${method}_${path}_handler`);
-    fn.handler(handlerCode);
-    // [ ] deals with options
-
-    // [ ] deal with layers
-    const options = this.currentAuthorizer ? { authorizer: this.currentAuthorizer } : undefined;
-    const integration = new ApiGateway.LambdaIntegration(fn.handlerFn); // {proxy: true}
-    this.api.root.resourceForPath(path)
-      .addMethod(method, integration, options);
-
-    this.currentHandler = fn.handlerFn;
-  }
-
-  get(path: string, handlerCode: string): RestApiConstruct {
-    this.createMethodIntegration('GET', path, handlerCode);
-    return this;
-  }
-
-  post(path: string, handlerCode: string): RestApiConstruct {
-    this.createMethodIntegration('POST', path, handlerCode);
-    return this;
-  }
-
-  put(path: string, handlerCode: string): RestApiConstruct {
-    this.createMethodIntegration('PUT', path, handlerCode);
-    return this;
-  }
-
-  delete(path: string, handlerCode: string): RestApiConstruct {
-    this.createMethodIntegration('DELETE', path, handlerCode);
-    return this;
-  }
+        return this;
+    }
 
 
-  options(path: string, handlerCode: string): RestApiConstruct {
-    this.createMethodIntegration('OPTIONS', path, handlerCode);
-    return this;
-  }
+    /**
+         * let the last created lambda hace read access to a given construct
+         *
+         * Supported targets:
+         * - DynamoDB
+         *
+         * @author Diego Tores <diegotorres0303@gmail.com>
+         *
+         * @param {Construct} construct
+         * @return {*}  {RestApiConstruct}
+         * @memberof RestApiConstruct
+         */
+    readFrom(construct: Construct): RestApiConstruct {
+        if (!this.currentHandler) throw new Error('you need to create a handler function first');
 
-  head(path: string, handlerCode: string): RestApiConstruct {
-    this.createMethodIntegration('HEAD', path, handlerCode);
-    return this;
-  }
+        // if Dynamo
+        const table = construct as Dynamo.Table;
+        table.grantReadData(this.currentHandler);
+
+
+        return this;
+    }
+
+    /**
+         * let the last created lambda hace write access to a given construct
+         *
+         * Supported targets:
+         * - DynamoDB
+         *
+         * @author Diego Tores <diegotorres0303@gmail.com>
+         *
+         * @param {Construct} construct
+         * @return {*}  {RestApiConstruct}
+         * @memberof RestApiConstruct
+         */
+    writeTo(construct: Construct): RestApiConstruct {
+        if (!this.currentHandler) throw new Error('you need to create a handler function first');
+
+
+        // if Dynamo
+        const table = construct as Dynamo.Table;
+        table.grantWriteData(this.currentHandler);
+
+
+        return this;
+    }
+
+    private createMethodIntegration(method: string, path: string, handlerCode: string, options?: FunctionOptions) {
+
+        console.log('use authorizer here', this.currentAuthorizer);
+        const fn = new FunctionConstruct(this, `${method}_${path}_handler`);
+        fn.handler(handlerCode, options);
+        // [ ] deals with options
+
+        // [ ] deal with layers
+        const integrationOptions = this.currentAuthorizer ? { authorizer: this.currentAuthorizer } : undefined;
+        const integration = new ApiGateway.LambdaIntegration(fn.handlerFn); // {proxy: true}
+        this.api.root.resourceForPath(path)
+            .addMethod(method, integration, integrationOptions);
+
+        this.currentHandler = fn.handlerFn;
+    }
+
+    get(path: string, handlerCode: string, options?: FunctionOptions): RestApiConstruct {
+        this.createMethodIntegration('GET', path, handlerCode, options);
+        return this;
+    }
+
+    post(path: string, handlerCode: string, options?: FunctionOptions): RestApiConstruct {
+        this.createMethodIntegration('POST', path, handlerCode, options);
+        return this;
+    }
+
+    put(path: string, handlerCode: string, options?: FunctionOptions): RestApiConstruct {
+        this.createMethodIntegration('PUT', path, handlerCode, options);
+        return this;
+    }
+
+    delete(path: string, handlerCode: string, options?: FunctionOptions): RestApiConstruct {
+        this.createMethodIntegration('DELETE', path, handlerCode, options);
+        return this;
+    }
+
+
+    options(path: string, handlerCode: string, options?: FunctionOptions): RestApiConstruct {
+        this.createMethodIntegration('OPTIONS', path, handlerCode, options);
+        return this;
+    }
+
+    head(path: string, handlerCode: string, options?: FunctionOptions): RestApiConstruct {
+        this.createMethodIntegration('HEAD', path, handlerCode, options);
+        return this;
+    }
 
 }
